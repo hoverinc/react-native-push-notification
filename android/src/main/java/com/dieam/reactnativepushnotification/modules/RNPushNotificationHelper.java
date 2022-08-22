c46149a9cbf2104d53336fcd13c2e2661473face
George Ellickson <64104901+GEllickson-Hover@users.noreply.github.com>
2022-08-22 12:12:26 -0700

    Use inexact alarms to avoid special use permission for Android 12

--- android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
+++ android/src/main/java/com/dieam/reactnativepushnotification/modules/RNPushNotificationHelper.java
@@ -132,11 +132,7 @@ public class RNPushNotificationHelper {
 
         Log.d(LOG_TAG, String.format("Setting a notification with id %s at time %s",
                 bundle.getString("id"), Long.toString(fireDate)));
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getAlarmManager().setExact(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
-        } else {
-            getAlarmManager().set(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
-        }
+        getAlarmManager().set(AlarmManager.RTC_WAKEUP, fireDate, pendingIntent);
     }
 
     public void sendToNotificationCentre(Bundle bundle) {
